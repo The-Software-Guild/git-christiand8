418c2070a2cd0390682367f37b48cec05a53417a
christiand8 <93944031+christiand8@users.noreply.github.com>
2021-11-21 00:18:47 -0500

    Add files via upload

---
+++ src/main/java/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.classroster.dao;
+
+import com.sg.classroster.dto.Student;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cd
+ */
+public class ClassRosterDaoFileImpl implements ClassRosterDao{
+    
+    public static final String ROSTER_FILE = "roster.txt";
+    public static final String DELIMITER = ": :";
+    
+    private Map<String, Student> students = new HashMap<>();
+
+    @Override
+    public Student addStudent(String studentID, Student student) 
+            throws ClassRosterPersistenceException {
+        loadRoster();
+        Student newStudent = students.put(studentID, student);
+        writeRoster();
+        return newStudent;
+    }
+
+    @Override
+    public List<Student> getAllStudents() 
+        throws ClassRosterPersistenceException {
+            loadRoster();
+            return new ArrayList(students.values());
+    }
+
+    @Override
+    public Student getStudent(String studentID)
+        throws ClassRosterPersistenceException {
+            loadRoster();
+            return students.get(studentID);
+    }
+
+    @Override
+    public Student removeStudent(String studentID)
+        throws ClassRosterPersistenceException {
+            loadRoster();
+            Student removedStudent = students.remove(studentID);
+            writeRoster();
+            return removedStudent;
+    }
+    
+    private Student unmarshallStudent(String studentAsText) {
+        
+    String[] studentTokens = studentAsText.split(DELIMITER);
+
+    String studentId = studentTokens[0];
+
+    Student studentFromFile = new Student(studentId);
+
+    studentFromFile.setFirstName(studentTokens[1]);
+
+    studentFromFile.setLastName(studentTokens[2]);
+
+    studentFromFile.setCohort(studentTokens[3]);
+
+    return studentFromFile;
+    }
+    
+    private void loadRoster() throws ClassRosterPersistenceException {
+    Scanner scanner;
+
+    try {
+        scanner = new Scanner(new BufferedReader(new FileReader(ROSTER_FILE)));
+    } 
+    catch (FileNotFoundException e) {
+        throw new ClassRosterPersistenceException("-_- Could not load roster data into memory.", e);
+    }
+    String currentLine;
+    Student currentStudent;
+    while (scanner.hasNextLine()) {
+        currentLine = scanner.nextLine();
+        currentStudent = unmarshallStudent(currentLine);
+
+        students.put(currentStudent.getStudentID(), currentStudent);
+    }
+    scanner.close();
+    }
+    
+    private String marshallStudent(Student aStudent){
+        
+    String studentAsText = aStudent.getStudentID() + DELIMITER;
+    studentAsText += aStudent.getFirstName() + DELIMITER;
+    studentAsText += aStudent.getLastName() + DELIMITER;
+    studentAsText += aStudent.getCohort();
+
+    return studentAsText;
+    }
+    
+private void writeRoster() throws ClassRosterPersistenceException {
+    PrintWriter out;
+
+    try {
+        out = new PrintWriter(new FileWriter(ROSTER_FILE));
+    } catch (IOException e) {
+        throw new ClassRosterPersistenceException(
+                "Could not save student data.", e);
+    }
+    
+    String studentAsText;
+    List<Student> studentList = this.getAllStudents();
+    for (Student currentStudent : studentList) {
+        studentAsText = marshallStudent(currentStudent);
+        out.println(studentAsText);
+        out.flush();
+    }
+    out.close();
+}
+}
+    
